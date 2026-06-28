15d7a5ef9c684a412396eb8f8cb33a0b1b3617c2
AppMod Upgrade <upgrade@appmod.local>
2026-06-28 17:42:49 +0530

    Initialize Spring Boot logistics marketplace project

---
+++ src/test/java/com/stp/shipmenttracking/ShipmenttrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stp.shipmenttracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmenttrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
